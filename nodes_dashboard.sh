284c3891d3a94f5e1f55bbd5bc420dd923d8e41d
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2023-08-09 14:05:39 +0200

    Alert manager + hardworker

---
+++ nodes_dashboard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+IFS=$'\n'
+set  +o xtrace  -o errexit  -o errtrace  -o nounset  -o pipefail  +o history
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source  "${SCRIPT_DIR}/lib.sh"
+showNotice "==== Executing $(basename "$0") ===="
+
+setContext
+set  -o xtrace
+getNodeIps
+
+talosctl  dashboard  --nodes "${NODE_IPS_COMMA}"
+
+showNotice "==== Finished $(basename "$0") ===="
