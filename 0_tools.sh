96cf1ada5a39d473e8366870769f33e97ff07f57
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2023-08-23 16:55:17 +0200

    Improved install

--- 0_tools.sh
+++ 0_tools.sh
@@ -10,33 +10,45 @@ set  -o xtrace
 if command -v brew > /dev/null; then
   showProgress "Install packages using Homebrew"
   brew  install  packer  hcloud  jq  siderolabs/talos/talosctl  kubernetes-cli  helm
+
+  showProgress "Install kubectl-mayastor from repo"
+  curl -L  "https://github.com/openebs/mayastor-control-plane/releases/latest/download/kubectl-mayastor-x86_64-apple-darwin.zip" \
+    --output /tmp/kubectl-mayastor.zip
+  unzip  -o  -d /tmp  /tmp/kubectl-mayastor.zip
+  sudo  install  -m 0755  /tmp/kubectl-mayastor  /usr/local/bin/kubectl-mayastor
 fi
 if command -v apt-get > /dev/null; then
   showProgress "Install packages using APT"
   DEB_BUILD_ARCH="$( dpkg --print-architecture )"
   sudo  apt-get  update
-  sudo  apt-get  install  --assume-yes  packer  hcloud-cli  jq
+  sudo  apt-get  install  --assume-yes  packer  jq
 
   showProgress "Install hcloud CLI from Github"
   DOWNLOAD_URL="$( curl -s https://api.github.com/repos/hetznercloud/cli/releases/latest | jq -r ".assets[] | select(.name==\"hcloud-linux-${DEB_BUILD_ARCH}.tar.gz\") | .browser_download_url" )"
-  curl -L "${DOWNLOAD_URL}" | tar -zxO hcloud | sudo bash -c 'cat > /usr/local/bin/hcloud'
-  sudo  chmod  +x  /usr/local/bin/hcloud
+  curl -L "${DOWNLOAD_URL}" | tar -zxO hcloud | sudo bash -c 'cat > /tmp/hcloud'
+  sudo  install  -m 0755  /tmp/hcloud  /usr/local/bin
 
   showProgress "Install talosctl from repo"
-  sudo  curl -L "https://github.com/siderolabs/talos/releases/download/${TALOS_VERSION}/talosctl-linux-${DEB_BUILD_ARCH}" \
-    --output /usr/local/bin/talosctl
-  sudo  chmod  +x  /usr/local/bin/talosctl
+  curl -L "https://github.com/siderolabs/talos/releases/download/${TALOS_VERSION}/talosctl-linux-${DEB_BUILD_ARCH}" \
+    --output /tmp/talosctl
+  sudo  install  -m 0755  /tmp/talosctl  /usr/local/bin
 
   showProgress "Install kubectl from repo"
-  sudo  curl -L  "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${DEB_BUILD_ARCH}/kubectl" \
-    --output /usr/local/bin/kubectl
-  sudo  chmod  +x  /usr/local/bin/kubectl
+  curl -L  "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${DEB_BUILD_ARCH}/kubectl" \
+    --output /tmp/kubectl
+  sudo  install  -m 0755  /tmp/kubectl  /usr/local/bin
 
   showProgress "Install Helm using get-helm-3 script"
-  sudo  curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 \
+  curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 \
     --output /tmp/get-helm
-  sudo  chmod  +x  /tmp/get-helm
+  chmod  +x  /tmp/get-helm
   sudo  /tmp/get-helm
+
+  showProgress "Install kubectl-mayastor from repo"
+  curl -L  "https://github.com/openebs/mayastor-control-plane/releases/latest/download/kubectl-mayastor-x86_64-linux-musl.zip" \
+    --output /tmp/kubectl-mayastor.zip
+  unzip  -o  -d /tmp  /tmp/kubectl-mayastor.zip
+  sudo  install  -m 0755  /tmp/kubectl-mayastor  /usr/local/bin/kubectl-mayastor
 fi
 
 showProgress "Show versions so we know the tools can be executed"
@@ -44,7 +56,8 @@ packer  version
 hcloud  version
 jq  --version
 talosctl  version  --client
-kubectl  version --client --output=yaml
+kubectl  version  --client
 helm  version
+kubectl  mayastor  --version
 
 showNotice "==== Finished $(basename "$0") ===="
